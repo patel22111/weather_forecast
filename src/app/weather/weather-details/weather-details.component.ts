554a46298f82a914b1b06655e6074425392d8a48
patel22111 <37548326+patel22111@users.noreply.github.com>
2023-08-29 13:10:53 +0530

    added styles

--- src/app/weather/weather-details/weather-details.component.ts
+++ src/app/weather/weather-details/weather-details.component.ts
@@ -1,10 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Forecast } from 'src/app/interfaces/forecast';
 
 @Component({
   selector: 'app-weather-details',
   templateUrl: './weather-details.component.html',
-  styleUrls: ['./weather-details.component.css'],
+  styleUrls: ['./weather-details.component.scss'],
 })
 export class WeatherDetailsComponent implements OnInit {
   @Input() forecast;
